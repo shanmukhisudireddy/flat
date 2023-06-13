2e68a9f7b8f4a73199db873d1f46e3a8538cc978
SUDIREDDY SHANMUKHI <113677353+shanmukhisudireddy@users.noreply.github.com>
2023-06-13 09:46:03 +0530

    Update login.php

--- admin/login.php
+++ admin/login.php
@@ -1,26 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
-
   <title>Admin | Online Food Ordering System</title>
-
   <?php include('./header.php'); ?>
   <?php include('./db_connect.php'); ?>
   <?php 
   session_start();
   if(isset($_SESSION['login_id']))
-  header("location:index.php?page=home");
-
+    header("location:index.php?page=home");
   $query = $conn->query("SELECT * FROM system_settings limit 1")->fetch_array();
-    foreach ($query as $key => $value) {
-      if(!is_numeric($key))
-        $_SESSION['setting_'.$key] = $value;
-    }
+  foreach ($query as $key => $value) {
+    if(!is_numeric($key))
+      $_SESSION['setting_'.$key] = $value;
+  }
   ?>
-
 </head>
 <style>
   body{
@@ -52,7 +47,7 @@
     align-items: center;
   }
   #login-right .card{
-    margin: auto
+    margin: auto;
   }
   .logo {
     margin: auto;
@@ -67,13 +62,10 @@
   .logo img{
     height: 80%;
     width: 80%;
-    margin: auto
+    margin: auto;
   }
 </style>
-
 <body>
-
-
   <main id="main" class=" bg-dark">
     <div id="login-left">
       <div class="logo">
@@ -83,7 +75,7 @@
     <div id="login-right">
       <div class="card col-md-8">
         <div class="card-body">
-          <form id="login-form" >
+          <form id="login-form">
             <div class="form-group">
               <label for="username" class="control-label">ID</label>
               <input type="text" id="username" name="username" class="form-control">
@@ -92,44 +84,44 @@
               <label for="password" class="control-label">Password</label>
               <input type="password" id="password" name="password" class="form-control">
             </div>
-            <center><button class="btn-sm btn-block btn-wave col-md-4 btn-primary">Login</button></center>
+            <center>
+              <button class="btn-sm btn-block btn-wave col-md-4 btn-primary">Login</button>
+            </center>
           </form>
         </div>
       </div>
     </div>
-
-
   </main>
-
-  <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
-
+  <a href="#" class="back-to-top">
+    <i class="icofont-simple-up"></i>
+  </a>
   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
   <script>
     $('#login-form').submit(function(e){
-      e.preventDefault()
-      $('#login-form button[type="button"]').attr('disabled',true).html('Logging in...');
-      if($(this).find('.alert-danger').length > 0 )
+      e.preventDefault();
+      $('#login-form button[type="button"]').attr('disabled', true).html('Logging in...');
+      if($(this).find('.alert-danger').length > 0)
         $(this).find('.alert-danger').remove();
       $.ajax({
-        url:'ajax.php?action=login',
-        method:'POST',
-        data:$(this).serialize(),
-        error:err=>{
-          console.log(err)
+        url: 'ajax.php?action=login',
+        method: 'POST',
+        data: $(this).serialize(),
+        error: err => {
+          console.log(err);
           $('#login-form button[type="button"]').removeAttr('disabled').html('Login');
         },
-        success:function(resp){
+        success: function(resp){
           if(resp == 1){
             location.href ='index.php?page=home';
-          }else if(resp == 2){
+          } else if(resp == 2){
             location.href ='voting.php';
-          }else{
-            $('#login-form').prepend('<div class="alert alert-danger">Username or password is incorrect.</div>')
+          } else {
+            $('#login-form').prepend('<div class="alert alert-danger">Username or password is incorrect.</div>');
             $('#login-form button[type="button"]').removeAttr('disabled').html('Login');
           }
         }
-      })
-    })
+      });
+    });
   </script>
 </body>
 </html>
